de9f4c29e84c470310985d33ad8fa5a364539f27
BenjaminBoutrois <benjamin.boutrois28@gmail.com>
2020-10-08 11:12:06 +0200

    Ajout fonctionnalités StudentDao

--- src/main/java/eu/ensup/jpaGestionEnsup/dao/StudentDao.java
+++ src/main/java/eu/ensup/jpaGestionEnsup/dao/StudentDao.java
@@ -3,6 +3,7 @@ package eu.ensup.jpaGestionEnsup.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 
 import eu.ensup.jpaGestionEnsup.domaine.Student;
 
@@ -22,9 +23,14 @@ public class StudentDao
 	
 	// Methods
 	
-	public void createStudent()
+	public void createStudent(Student student)
 	{
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
 		
+		entityManager.persist(student);
+		
+		entityTransaction.commit();
 	}
 	
 	public Student getStudent(int id)
@@ -43,4 +49,37 @@ public class StudentDao
 				.findFirst().get();
 	}
 
+	public List<Student> getAllStudents()
+	{
+		return entityManager.createQuery("SELECT s FROM Student s", Student.class).getResultList();
+	}
+	
+	public void deleteStudent(int id)
+	{
+		Student student = getStudent(id);
+		
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		entityManager.remove(student);
+		
+		entityTransaction.commit();
+	}
+	
+	public void updateStudent(Student newStudent)
+	{
+		Student oldStudent = getStudent(newStudent.getId());
+		
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		oldStudent.setFirstName(newStudent.getFirstName());
+		oldStudent.setLastName(newStudent.getLastName());
+		oldStudent.setAddress(newStudent.getAddress());
+		oldStudent.setMailAddress(newStudent.getMailAddress());
+		oldStudent.setNumberPhone(newStudent.getNumberPhone());
+		oldStudent.setCourses(newStudent.getCourses());
+		
+		entityTransaction.commit();
+	}
 }
--- src/main/java/eu/ensup/jpaGestionEnsup/service/StudentService.java
+++ src/main/java/eu/ensup/jpaGestionEnsup/service/StudentService.java
@@ -1,14 +1,55 @@
 package eu.ensup.jpaGestionEnsup.service;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import eu.ensup.jpaGestionEnsup.dao.StudentDao;
+import eu.ensup.jpaGestionEnsup.domaine.Student;
 
 public class StudentService
 {
+	// Fields
+	
 	private StudentDao studentDao;
 
-	public StudentService()
+	// Constructors
+	
+	public StudentService(EntityManager entityManager)
 	{
-		
+		super();
+		studentDao = new StudentDao(entityManager);
 	}
 
+	// Methods
+	
+	public void createStudent(Student student)
+	{
+		studentDao.createStudent(student);
+	}
+	
+	public Student getStudent(int id)
+	{
+		return studentDao.getStudent(id);
+	}
+	
+	public Student getStudentByMail(String mail)
+	{
+		return studentDao.getStudentByMail(mail);
+	}
+	
+	public List<Student> getAllStudents()
+	{
+		return studentDao.getAllStudents();
+	}
+	
+	public void deleteStudent(int id)
+	{
+		studentDao.deleteStudent(id);
+	}
+	
+	public void updateStudent(Student student)
+	{
+		studentDao.updateStudent(student);
+	}
 }
