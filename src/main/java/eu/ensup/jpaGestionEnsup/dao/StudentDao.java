bb86a3ff418b33548bf3fcc84100c9c3dfe1a7ab
BenjaminBoutrois <benjamin.boutrois28@gmail.com>
2020-10-08 10:36:58 +0200

    En cours de développement

--- src/main/java/eu/ensup/jpaGestionEnsup/dao/StudentDao.java
+++ src/main/java/eu/ensup/jpaGestionEnsup/dao/StudentDao.java
@@ -1,9 +1,46 @@
 package eu.ensup.jpaGestionEnsup.dao;
 
-public class StudentDao {
+import java.util.List;
 
-	public StudentDao() {
-		// TODO Auto-generated constructor stub
+import javax.persistence.EntityManager;
+
+import eu.ensup.jpaGestionEnsup.domaine.Student;
+
+public class StudentDao
+{
+	// Fields
+	
+	private EntityManager entityManager;
+	
+	// Constructors
+
+	public StudentDao(EntityManager entityManager)
+	{
+		super();
+		this.entityManager = entityManager;
+	}
+	
+	// Methods
+	
+	public void createStudent()
+	{
+		
+	}
+	
+	public Student getStudent(int id)
+	{
+		return entityManager.find(Student.class, id);
+	}
+	
+	public Student getStudentByMail(String mail)
+	{
+		List<Student> students = entityManager.createQuery("SELECT s FROM Student s", Student.class).getResultList();
+		
+		return
+				students
+				.stream()
+				.filter(s -> s.getMailAddress().equals(mail))
+				.findFirst().get();
 	}
 
 }
---
+++ src/main/java/eu/ensup/jpaGestionEnsup/service/StudentService.java
@@ -0,0 +1,14 @@
+package eu.ensup.jpaGestionEnsup.service;
+
+import eu.ensup.jpaGestionEnsup.dao.StudentDao;
+
+public class StudentService
+{
+	private StudentDao studentDao;
+
+	public StudentService()
+	{
+		
+	}
+
+}
